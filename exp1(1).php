5db03b0151f80d01ac0991fb7c113a409e901bdd
Avijeet <54774303+avijeet25@users.noreply.github.com>
2019-09-30 23:59:34 +0530

    Add files via upload

---
+++ exp1(1).php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My CV</title>
+		<style>
+			p.one {
+  					border-style: solid;
+  					border-color: red;
+					}
+			p.two {
+  					border-style: solid;
+  					border-color: green;
+					} 
+			p.three {
+  					border-style: solid;
+  					border-color: red green blue yellow;
+					}
+			h2.high	{
+					font-family: Cambria;
+					background-color: #dddddd;
+					}
+			table 	{
+  					font-family: arial, sans-serif;   border-collapse: collapse;   width: 100%;
+					}
+			td, th {
+  					border: 1px solid #dddddd;   text-align: left;   padding: 8px;
+					}
+			div.a
+					{
+					padding: 35px 70px;
+					}
+			.img: 
+					{
+					align:right;
+					}
+			.rotateimg270
+					{
+ 					-webkit-transform:rotate(270deg);
+  					-moz-transform: rotate(270deg);
+  					-ms-transform: rotate(270deg);
+  					-o-transform: rotate(270deg);
+  					transform: rotate(270deg);
+					}
+			
+			</style>
+	</head>
+	
+	<body style = "background-color: rgba(0,0,255,0.3);">
+		
+		<img src = "my_pic1.jpg" alt =" Avijeet's Pic" style = "float:right" width = "18%" height = "15%"  >
+		<h1 style = "font-size:60px; font-family: Abyssinica SIL , sans-serif;">AVIJEET KUMAR</h1>
+		
+		
+		<h2><u><b>Address of Correspondence</b></u></h2>
+		<p>University of Petroleum and Energy Studies<br>
+			Energy Acres PO Bidholi via Prem Nagar<br>
+			Dehradun-248007(Uttarakhand),India.<br>
+		</p>
+			<br>
+		<h3><b>Phone: </b>+91 9565795888</h3>
+		<h3><b>Email: </b>500067577@stu.upes.ac.in</h3>
+				<br>
+					<br>
+						
+		<h2 class="high">CAREER OBJECTIVE</h2>
+		<p><i>To excel in my chosen field in a job that both challenges and inspires me, while pushing me to work to the best of my abilities and produce the highest quality work that I am capable of.</i></p>
+		<br>
+		<h2 class="high">ACADEMIC BACKGROUND</h2>
+
+		<table>
+			
+			<tr>		<th>Year(s)</th>		<th>Qualification</th>			<th>Board/University</th>		<th>College/Institue/University</th>		<th>Percentage/CGPA</th>		</tr>
+			<tr>		<td>2016</td>		<td>10th</td>			<td>ICSE</td>		<td>City Montessori Inter College</td>		<td>100%</td>		</tr>
+			<tr>		<td>2018</td>		<td>12th</td>			<td>ISC</td>		<td>City Montessori Inter College</td>		<td>100%</td>		</tr>
+			
+		</table>			
+				
+			<h2 class="high" > TRAINING & PROJECTS UNDERTAKEN</h2>
+			<ul>
+				<li>CLOUD WORKSHOP AND TRAINING PROGRAM BY MICROSOFT TECHNICAL COMUNITY</li>
+				<li>LINKEDIN AND CV BUILDING WORKSHOP</li>
+				<li>MICROSOFT AZURE WORKSHOP CONDUCTED BY MICROSOFT TECHNICAL COMUNITY</li>
+				<li>GITHUB WORKSHOP BY CODING NINJA'S</li>
+			</ul>
+		<h2 class = "high">COMPUTER SKILLS</h2>
+		<h3><b>LANGUAGES : C, C++, HTML, CSS(Basic), PHP.<br> 
+				OTHERS   : MySQL, Linux, MS Office.<b></h3> 
+			<br>
+				<h2 class = "high">PERSONAL DETAILS</h2>
+				<table style = "font-size:18px;">
+					<tr>		<td>Date Of Birth</td>		<td>:</td>		<td><b>22/11/2000</b></td>		</tr>
+					<tr>		<td>Father's Name</td>		<td>:</td>		<td><b>Mr. Amit Kumar Srivastava</b></td>		</tr>
+					<tr>		<td>Mother's Name</td>		<td>:</td>		<td><b>Mrs. Shikha</b></td>		</tr>
+					<tr>		<td>Category</td>		<td>:</td>			<td>GENERAL</td>		</tr>
+					<tr>		<td>Gender</td>		<td>:</td>		<td><b>Male</b></td>		</tr>
+					<tr>		<td>Nationality</td>		<td>:</td>		<td><b>Indian</b></td>		</tr>
+					<tr>		<td>Home Town</td>		<td>:</td>		<td><b>Lucknow</b></td>		</tr>
+					<tr>		<td>Permanent Address</td>		<td>:</td>		<td><b>8/47, Radheyshyam Bhavan, Purana Quila, Lucknow, UttarPradesh</b></td>		</tr>
+					<tr>		<td>Phone No.</td>		<td>:</td>		<td><b>9565795888</b></td>		</tr>
+					<tr>		<td>Languages Known</td>		<td>:</td>		<td><b>Hindi,English</b></td>		</tr>
+					
+					
+					
+					
+	</body>
+</html>
